a30c85732b14d5d6df6f416acb37d078d2418bf3
Rafael Romão <rafaelromao@gmail.com>
2021-12-14 11:31:37 -0300

    fix copyright

--- keyboards/xiudi/xd75/keymaps/rafaelromao/keymap.c
+++ keyboards/xiudi/xd75/keymaps/rafaelromao/keymap.c
@@ -1,19 +1,4 @@
-/* Copyright 2017 Wunder
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/* Copyright 2021 Rafael Romão https://github.com/rafaelromao
+/* Copyright 2021 Rafael Romão @rafaelromao
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
